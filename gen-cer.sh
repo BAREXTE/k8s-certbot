e3c163b2a87c505703632e0812346921d22cff39
BAREXTE <46282149+BAREXTE@users.noreply.github.com>
2020-07-18 11:41:17 +0200

    Update gen-cer.sh

--- gen-cer.sh
+++ gen-cer.sh
@@ -4,4 +4,4 @@ domains_arr=($CERT_DOMAINS)
 req_cn=${domains_arr[0]}
 req_email=$CERT_EMAIL
 
-certbot certonly -m $req_email --webroot --webroot-path /usr/share/nginx/html --csr /usr/share/easy-rsa/pki/reqs/$req_cn.req
+certbot certonly --agree-tos -m $req_email --webroot --webroot-path /usr/share/nginx/html --csr /usr/share/easy-rsa/pki/reqs/$req_cn.req
